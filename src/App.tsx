888bb1a5cdd99904802a128aacce1fa3a90c997d
Maciej Skurznica <maciej.skurznica@gmail.com>
2023-04-03 11:33:57 +0100

    BL-1 add global theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Landing, Sidebar } from "src/components";
 import { GlobalStyle } from "src/styles/global";
+import { primaryTheme } from "src/styles/themes";
+import { ThemeProvider } from "styled-components";
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Sidebar />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={primaryTheme}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/themes.ts
@@ -0,0 +1,10 @@
+export const primaryTheme = {
+  teal: "#93B4BC",
+  white: "#FFFFFF",
+  navy: "#454664",
+  lightNavy: "#E9EDF6",
+  sidebarButtonGradient: {
+    from: "#679CF6",
+    to: "#4072EE"
+  }
+};

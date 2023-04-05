876b14fc0aad4ce29029876fc343b96f57bcf2ea
Maciej Skurznica <maciej.skurznica@gmail.com>
2023-04-05 02:20:09 +0100

    BL-10 fix styling

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,7 +11,7 @@ export const GlobalStyle = createGlobalStyle<{
 }>`
   body {
     margin: 0;
-    height: 100vh;
+    min-height: 100vh;
     display: flex;
     justify-content: center;
     background-color: ${({ theme }) => theme.bgGrey};
